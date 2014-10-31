a11cfeed779b13f12743648ae5052fa87b14a691
Maria Miller <mia2110@gmail.com>
2014-10-31 18:05:35 -0400

    Create nodes and graph

---
+++ treasure-hunter.cpp
@@ -0,0 +1,15 @@
+// treasure-hunter.cpp
+// Jason McGough, Maria Miller
+// Artificial Intelligence - Fall 2014
+
+#include <iostream>
+
+#include "treasure-hunter.h"
+
+int main() {
+
+ Node N1;
+
+ 
+
+} // end main()

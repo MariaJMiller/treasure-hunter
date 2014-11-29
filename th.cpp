91b0bccf580c424b69508fc46d0c492350982a3e
Maria Miller <mia2110@gmail.com>
2014-11-29 09:58:46 -0500

    Fix syntax errors preventing compile

--- th.cpp
+++ th.cpp
@@ -6,35 +6,16 @@
  
 #include <iostream>
 
-#include "th.h"
+//#include "th.h"
 #include "maze.h"
 #include "ai.h"
 
 
 int main() {
 
-  Maze Maze1;
+  Maze maze1;
 
   int i, j = 0;
-
-  Maze1.placeWalls();
-  Maze1.placeExit();
-  Maze1.placeTrsr();
-
-  // ERROR CHECKING** Cout array values 
-  for(i = 0; i < ROWS; ++i) {
-    for(j = 0; j < COLS; ++j) {
-      std::cout << Maze1.smMaze[i][j] << " ";
-    }
-    std::cout << std::endl;
-  }
-
-
-
-
-
-                                                    
-
-
+                                            
 } // end main()
 
--- th.h
+++ th.h
@@ -7,23 +7,6 @@
  
 #ifndef TH_H
 #define TH_H
-
-#include <iostream>
-#include <random>
-
-
-/* Generate random number */
-inline int randomGen() {
-
-  // Seed with a real random value, if available
-  std::random_device rd;
  
-  // Choose a random mean between 0 and 6
-  std::default_random_engine e1(rd());
-  std::uniform_int_distribution<int> uniform_dist(0, 6);
-  return uniform_dist(e1);
-
-}
-
 #endif
 
